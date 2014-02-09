70a5821127f926d8d5a4b98f8d2dc6143511ca67
Agam Rafaeli <agam.rafaeli@gmail.com>
2014-02-09 21:25:05 +0200

    Deck file

--- Deck.py
+++ Deck.py
@@ -1,4 +1,22 @@
-class Deck
+import random
+
+class Deck:
+
+	def suits(self):
+		return ["Heart","Diamond","Spade","Clover"]
+
+	def numbers(self):
+		return range(2,15);
 
 	def __init__(self):
-		print "Hello"
+		cards = []
+		for suit in self.suits():
+			for number in self.numbers():
+				cards.append((number,suit))
+		random.shuffle(cards,random.random)
+		self.cards = cards
+
+
+	def dealCard(self):
+		dealtCard = self.cards.pop()
+		return dealtCard
