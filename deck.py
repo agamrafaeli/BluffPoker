0609bfedbe9b3099c71512f8f4dc0d9b050b03da
Asaf Liveanu <aliveanu@gmail.com>
2014-02-09 21:43:03 +0200

    asaf stupid commit

--- deck.py
+++ deck.py
@@ -2,19 +2,19 @@ import random
 
 class Deck:
 
-	def suits(self):
-		return ["Heart","Diamond","Spade","Clover"]
+    def suits(self):
+        return ["Heart","Diamond","Spade","Clover"]
 
-	def numbers(self):
-		return range(2,15);
+    def numbers(self):
+        return range(2,15);
 
-	def __init__(self):
-		cards = []
-		for suit in self.suits():
-			for number in self.numbers():
-				cards.append((number,suit))
-		random.shuffle(cards,random.random)
-		self.cards = cards
+    def __init__(self):
+        cards = []
+        for suit in self.suits():
+            for number in self.numbers():
+                cards.append((number,suit))
+        random.shuffle(cards,random.random)
+        self.cards = cards
 
 	def dealCard(self):
 		dealtCard = self.cards.pop()
