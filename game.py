8de0c76fd67f6986bb365e5c1bef9ad2ee40ae46
Agam Rafaeli <agam.rafaeli@gmail.com>
2014-02-10 15:50:13 +0200

    poker players barebones and simple game

--- game.py
+++ game.py
@@ -1,28 +1,30 @@
 import random, poker
 from deck import Deck
+from pokerPlayers import pokerPlayerFactory
 
 
 class Game:
 
 	def __init__(self,playerArr):
-
+		print "*****STARTING GAME*****"
 		self.optionalHands = poker.initOptions()
 		self.currentHand = None
 
+		print "*****CREATING PLAYERS*****"
+		playerFactory = pokerPlayerFactory()
+
 		self.players = []
 		for player in playerArr:
-			newPlayer =
-			newPlayer['name'] = player['name']
-			newPlayer['agent'] = player['agent']
-			newPlayer['numCards'] = 5
-			newPlayer['currentHand'] = None
+			playerName = player[0]
+			playerType = player[1]
+			newPlayer = playerFactory.newPokerPlayer(playerName,playerType)
 			self.players.append(newPlayer)
 		random.shuffle(self.players,random.random)
 
+		print "*****DEALING CARDS*****"
 
-		winner = 0
-		while self.noWinner():
-			winner = self.playSingleRound(winner)
+		print "The player to go first is "+self.players[0].name
+
 
 	def playSingleRound(self,startingPlayer): pass
 
