6ce46d5124e1855ae7f1d407bde830b7e55ce0ab
Agam Rafaeli <agam.rafaeli@gmail.com>
2014-02-10 15:26:51 +0200

    simplified game

--- game.py
+++ game.py
@@ -6,6 +6,9 @@ class Game:
 
 	def __init__(self,playerArr):
 
+		self.optionalHands = poker.initOptions()
+		self.currentHand = None
+
 		self.players = []
 		for player in playerArr:
 			newPlayer =
@@ -16,44 +19,14 @@ class Game:
 			self.players.append(newPlayer)
 		random.shuffle(self.players,random.random)
 
-		self.optionalHands = poker.optionalHands()
-		self.currentHand = None
+
 		winner = 0
 		while self.noWinner():
 			winner = self.playSingleRound(winner)
 
-	def playSingleRound(self,startingPlayer):
-		roundDeck = deck()
-		currentPlayer = int(startingPlayer)
-		for player in self.players():
-			#Only elgible players??
-			for i in xrange(player['numCards']):
-				newCard = roundDeck.dealCard()
-				player['currentHand'].append(newCard)
-		while True:
-			(wasChallenge,losingPlayerIndex) = self.playSingleTurn(currentPlayer)
-			self.players[losingPlayerIndex]['numCards'] -= 1
-			if wasChallenge:
-				break
-			currentPlayer = (currentPlayer + 1) % len(self.players)
+	def playSingleRound(self,startingPlayer): pass
 
+	def playSingleTurn(self,announcingPlayerIndex): pass
 
-	def playSingleTurn(self,announcingPlayerIndex):
-		announcedHand = self.players[announcingPlayerIndex]['agent'].announce(self)
-		challenged = False
-		for i in xrange(announcingPlayerIndex,len(self.players)):
-			if self.players[i]['agent'].challenge(self):
-				challengingPlayerIndex = i
-				challenged = True
-				break
-		if challenged:
-			cardsInPlay = []
-			for player in self.players:
-				cardsInPlay = cardsInPlay + list(player['currentHand'])
-				if poker.legalHand(cardsInPlay,self.currentHand):
-					return (True,challengingPlayerIndex)
-				else
-					return (True,announcingPlayerIndex)
-		return (False,-1)
-
-
+	def getOptionalHands():
+		return list(self.optionalHands)
