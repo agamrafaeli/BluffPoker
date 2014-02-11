da4afd221ab8a05a882abfc4e6b71bc3ec19d933
Asaf Liveanu <aliveanu@gmail.com>
2014-02-11 13:22:55 +0200

    poker: #returns the actual possible strongest hand     def getStrongestHandFromHand(self,hand):
    #return int value (strength) of the hand
        def getHandStrength(self,hand):
    
    #Which hand is stronger = What hand can create the strongest combination
        def handStandoff(self,hand1,hand2):
    
    #Returns the best weakest hand that the list of vertain hands has that beats the hand to beat
        def getBestWeakestHandToAnnounce(self,listOfCertainHands,handToBeat):
    
    pokerPlayers:
    catuios bluffer
    big mouth

--- poker.py
+++ poker.py
@@ -35,25 +35,25 @@ class Poker:
 
         return handsOptions
 
-    def getAllHighCards(numbersInDeck):
+    def getAllHighCards(self,numbersInDeck):
         highCardsList = []
         for num in numbersInDeck:
             highCardsList.append([(num,1)])
         return highCardsList
 
-    def getAllPairs(numbersInDeck):
+    def getAllPairs(self,numbersInDeck):
         pairs=[]
         for pairNumber in numbersInDeck:
             pairs.append([(pairNumber,2)])
         return pairs
 
-    def getAllTriplets(numbersInDeck):
+    def getAllTriplets(self,numbersInDeck):
         triplets=[]
         for tripNumber in numbersInDeck:
             triplets.append([(tripNumber,3)])
         return triplets
 
-    def getAllDoublePair(allSinglePairs):
+    def getAllDoublePair(self,allSinglePairs):
         doublePairList = []
         secondPairOptions = list (allSinglePairs)
         for firstPair in allSinglePairs:
@@ -65,13 +65,13 @@ class Poker:
                 doublePairList.append(singleDoubleTemp)
         return doublePairList
 
-    def getAllStraightOptions(numbersInDeck):
+    def getAllStraightOptions(self,numbersInDeck):
         straightList=[]
         for firstNum in xrange(2,11):
             straightList.append([(firstNum,1),(firstNum+1,1),(firstNum+2,1),(firstNum+3,1),(firstNum+4,1)])
         return straightList
 
-    def getAllFullHouseOptions(pairOptions, tripleOptions):
+    def getAllFullHouseOptions(self,pairOptions, tripleOptions):
         fullHouseList =[]
         for pair in pairOptions:
             for triple in tripleOptions:
@@ -83,7 +83,7 @@ class Poker:
                 fullHouseList.append(singleFullHouseTuple)
         return fullHouseList
 
-    def getAllFoursome(numbersInDeck):
+    def getAllFoursome(self,numbersInDeck):
         foursomeList = []
         for num in numbersInDeck:
             foursomeList.append([(num,4)])
@@ -97,7 +97,7 @@ class Poker:
     def printHandOptions(self):
         print self.initOptions()
 
-    def standOff(cardsOnTable,announcement):
+    def standOff(self,cardsOnTable,announcement):
 
         for combination in announcement:
             ifCombinationExists = False
@@ -113,3 +113,60 @@ class Poker:
             else:
                 return False
         return True
+
+    def myCertainHands(self,hand):
+        certainHandsList = []
+        for optionalHand in self.initHandsOptions():
+            if (self.standOff(hand,optionalHand)):
+                certainHandsList.append(optionalHand)
+        return certainHandsList
+
+    def isOptionalHAnd(self,hand,optionalHand):
+        for cardInHand in hand:
+            for cardInOptionalHand in optionalHand:
+                if cardInHand[0]==cardInOptionalHand[0]:
+                    return True
+
+    def myOptionalHands(self,hand):
+        optionalHandsList = []
+        for optionalHand in self.initHandsOptions():
+            if (self.isOptionalHAnd(hand,optionalHand)):
+                optionalHandsList.append(optionalHand)
+        print optionalHandsList
+        return optionalHandsList
+
+#returns the actual possible strongest hand
+    def getStrongestHandFromHand(self,hand):
+        handCertainHands = self.myCertainHands(hand)
+        return handCertainHands[len(handCertainHands)-1]
+
+
+#return int value (strength) of the hand
+    def getHandStrength(self,hand):
+        count = 1
+        for handToCheck in self.initHandsOptions():
+            if hand==handToCheck:
+                return count
+            count += 1
+
+
+#Which hand is stronger = What hand can create the strongest combination
+    def handStandoff(self,hand1,hand2):
+        if self.getHandStrength(self.getStrongestHandFromHand(hand1)) < \
+                self.getHandStrength(self.getStrongestHandFromHand(hand2)):
+            return True
+        return False
+
+
+#Returns the best weakest hand that the list of vertain hands has that beats the hand to beat
+    def getBestWeakestHandToAnnounce(self,listOfCertainHands,handToBeat):
+        for hand in listOfCertainHands:
+            if self.getHandStrength(hand) > self.getHandStrength(handToBeat):
+                return hand
+        return ([()])
+p = Poker()
+
+print p.getBestWeakestHandToAnnounce(p.myCertainHands([(6,2),(7,3)]),[(6,1)])
+
+
+
--- pokerPlayers.py
+++ pokerPlayers.py
@@ -1,139 +1,150 @@
 from random import random
+from poker import Poker
+
 
 class pokerPlayerFactory(object):
-	def newPokerPlayer(self,name,playerType):
-		if playerType == "CONSERVATIVE": 		return ultraConservativePlayer(name)
-		if playerType == "BIG_MOUTH": 			return bigMouthPlayer(name)
-		if playerType == "CAUTIOUS_BLUFFER":	return cautiousBlufferPlayer(name)
-		if playerType == "HUMAN":				return humanPlayer(name)
-		if playerType == "HEURISTIC_PLAYER":	return heuristicPlayer(name)
-		if playerType == "LEARNING_PLAYER":		return learningPlayer(name)
+    def newPokerPlayer(self,name,playerType):
+        if playerType == "CONSERVATIVE": 		return ultraConservativePlayer(name)
+        if playerType == "BIG_MOUTH": 			return bigMouthPlayer(name)
+        # if playerType == "CAUTIOUS_BLUFFER":	return cautiousBlufferPlayer(name)
+        # if playerType == "HUMAN":				return humanPlayer(name)
+        # if playerType == "HEURISTIC_PLAYER":	return heuristicPlayer(name)
+        # if playerType == "LEARNING_PLAYER":		return learningPlayer(name)
 
 
 class pokerPlayer(object):
-	def __init__(self,name):
-		self.cardsInHand = [];
-		self.cardsLeft = 5
-		self.name = name;
-		self.lowestHand = None
-		self.highestHand = None
+    def __init__(self,name):
+        self.cardsInHand = [];
+        self.playersHand=[];
+        self.cardsLeft = 5
+        self.name = name;
+        self.lowestHand = None
+        self.highestHand = None
+        self.currentAnnouncedHand=[]
 
-	def hasHand(self,hand):
-		for demand in hand:
-			countInPlayer = 0
-			for card in self.cardsInHand:
-				if card[0] == demand[0]:
-					countInPlayer += 1
-			if countInPlayer < demand[1]:
-				return False
-		return True
 
-	def announce(self,game): pass
 
-	def challenge(self,game): pass
+    def getCurrentAnnouncedHand(self):
+        return  self.currentAnnouncedHand
 
-	def learn(self,game): pass
+    def setCurrentAnnouncedHAnd(self, hand):
+        self.currentAnnouncedHand = hand
 
-	def calibrateStrategy(self,game):
-		for hand in game.getRemainingHands():
-			if self.hasHand(hand):
-				self.lowestHand = list(hand)
-				break
-		for hand in list(game.getRemainingHands()).reverse():
-			if self.hasHand(hand):
-				self.highestHand = list(hand)
-				break
+    def hasHand(self,hand):
+        for demand in hand:
+            countInPlayer = 0
+            for card in self.cardsInHand:
+                if card[0] == demand[0]:
+                    countInPlayer += 1
+            if countInPlayer < demand[1]:
+                return False
+        return True
 
-	def recieveCard(self,card):
-		self.cardsInHand.append(card)
+    def announce(self,game): pass
 
-	def lostGame():
-		self.cardsLeft -= 1
+    def challenge(self,game): pass
+
+    def learn(self,game): pass
+
+    def calibrateStrategy(self,game):
+        for hand in game.getRemainingHands():
+            if self.hasHand(hand):
+                self.lowestHand = list(hand)
+                break
+        for hand in list(game.getRemainingHands()).reverse():
+            if self.hasHand(hand):
+                self.highestHand = list(hand)
+                break
+
+    def recieveCard(self,card):
+
+        existInHand = False
+        for cardVal in self.playersHand:
+            if card[0]==cardVal[0]:
+                cardVal[1] += 1
+                existInHand = True
+
+        if not existInHand:
+            self.playersHand.append(([card[0],1]))
+
+
+        self.cardsInHand.append(card)
+
+    def lostGame(self):
+        self.cardsLeft -= 1
 
 class bigMouthPlayer(pokerPlayer):
 
-	def __init__(self,name):
-		super(bigMouthPlayer,self).__init__(name)
-		print "Created bigMouthPlayer named "+self.name
-		self.challengedCounter = 0
+    def __init__(self,name):
+        super(bigMouthPlayer,self).__init__(name)
+        print "Created bigMouthPlayer named "+ self.name
+        self.challengedCounter = 0
 
-	def announce(self,game):
-		return game.getRemainingHands()[0]
+    def announce(self,game):
+        return game.getRemainingHands()[0]
+        #check what was anounced till now
+        #Can he win the last announced hand?
+            #if can, anounce the next strong hand he can anounce and back up
 
-	def challenge(self,game):
-		self.challengedCounter += 1
-		challengeProbability = -0.5
-		for i in xrange(1,self.challengedCounter):
-			challengeProbability += (1.0/j) - (1.0/(j+1.0))
-		return random.random() <= challengeProbability
-
-
-class humanPlayer(pokerPlayer):
-
-	def __init(self,name):
-		super(bigMouthPlayer,self).__init__(name)
-		print "Created human player named "+self.name
-
-	def announce(self,game):
-		self.printStats(game)
-
-		print "To select:"
-		print "0 for high card.\t 1 for pair. \t\t2 for two pairs."
-		print "3 for triplets.\t\t 4 for straight.\t5 for full house."
-		print "6 for quad."
-		typeOfHand = int(raw_input("Type of hand you are announcing: "))
-		secondQuestionFlag = False
-		if typeOfHand == 0:
-			value = int(raw_input("High card at what level? "))
-			return [(value,1)]
-		if typeOfHand == 1:
-			value = int(raw_input("A pair of what? "))
-			return [(value,2)]
-		if typeOfHand == 2:
-			firstValue = int(raw_input("The first pair is of what value? "))
-			secondValue = int(raw_input("And the second pair? "))
-			return [(firstValue,2),(secondValue,2)]
-		if typeOfHand == 3:
-			value = int(raw_input("The triplets are of what value? "))
-			return [(value,3)]
-		if typeOfHand == 4:
-			value = int(raw_input("What is the highest value of the straight? "))
-			return [(value-4,1),(value-3,1),(value-2,1),(value-1,1),(value,1)]
-		if typeOfHand == 5:
-			firstValue = int(raw_input("What value are the triplets? "))
-			secondValue = int(raw_input("And what value is the pair? "))
-			return [(firstValue,3),(secondValue,2)]
-		if typeOfHand == 6:
-			value = int(raw_input("What value is the quad?"))
-			return [(value,4)]
+    def challenge(self,game):
+        self.challengedCounter += 1
+        challengeProbability = -0.5
+        for i in xrange(1,self.challengedCounter):
+            challengeProbability += (1.0/i) - (1.0/(i+1.0))
+        return random.random() <= challengeProbability
 
 
-		print "###################ERROR###################"
-		print "BAD TYPE OF HAND ENTERED"
-		raw_input("Ctrl+c to break the program")
+class ultraConservativePlayer(pokerPlayer):
 
-	def printStats(self,game):
-		print "#######################"
-		playerStatLine = ""
-		for player in game.players:
-			playerStatLine += player.name+": "+str(player.cardsLeft)+"\t\t"
-		print "Cards left in hands of other players:"
-		print playerStatLine
-		print "Your cards are:"
-		cardLine = ""
-		for card in self.cardsInHand:
-			cardLine += str(card[0])+" of "+card[1]+"s,"
-		print cardLine
-		#TODO: Need to add current hand
+    #works with certain and challenges if last anouncement is better then his best certain hand
 
-	def challenge(self,game):
-		self.printStats(game)
+    def __init__(self,name):
+        super(ultraConservativePlayer,self).__init__(name)
+        print "Created ultra Conservative Player named "+self.name
+        self.challengedCounter = 0
 
-		print "Are you challenging this current hand?"
-		print "1 for YES. \tSomething else for no:\t"
-		value = int(raw_input(""))
-		if value == 1:
-			return True
-		return False
+    def announce(self,game):
+        poker = Poker()
+        handToAnnounce = poker.getBestWeakestHandToAnnounce(poker.myCertainHands(),self.getCurrentAnnouncedHand())
+        return handToAnnounce
+
+    def challenge(self,game):
+        handAnnounced = self.getCurrentAnouncedHand()
+        poker = Poker()
+        highestAvailableHand = poker.myCertainHands()[len(game.getRemainingHands()-1)]
+        if poker.handStandoff(handAnnounced,highestAvailableHand):
+            #The player will not challenge because he is certain to have a hand in hand that
+            # is stronger then what was announced
+            return False
+        self.challengedCounter += 1
+        return True
+
+
+
+class cautiousBlufferPlayer(pokerPlayer):
+    #Calls ascending hands continuously. With increasing probability calls "Bluff!"
+    #Asaf will do this
+
+    def __init__(self,name):
+        super(cautiousBlufferPlayer,self).__init__(name)
+        print "Created Cautious Bluffer Player named "+self.name
+        self.challengedCounter = 0
+
+    def announce(self,game):
+        poker = Poker()
+
+        # says the next best hand he can say after the current anouncement
+        handToAnnounce = poker.getBestWeakestHandToAnnounce(poker.initHandsOptions(),self.getCurrentAnnouncedHand())
+        return handToAnnounce
+
+    def challenge(self,game):
+        self.challengedCounter += 1
+        challengeProbability = 0.025
+        for i in xrange(1,self.challengedCounter):
+            challengeProbability += 0.025
+
+        return random.random() <= challengeProbability
+
+
 
 
